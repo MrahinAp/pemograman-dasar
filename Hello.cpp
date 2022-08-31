57ee4c33f79b26ec66d6941c6e37fdd45c73c549
MrahinAp <111964792+MrahinAp@users.noreply.github.com>
2022-08-31 14:08:54 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main () 
+{
+	cout << "Hello World" << endl ;
+	cout << "Nama  : M.Rahin.A.P" << endl ;
+	cout << "Kelas : X PPLG 1" << endl ;
+	cout << "====================" << endl ;
+	cout << " Angka 1 = 10" << endl;
+	cout << " Angka 2 = 5" << endl;
+	cout << "hasil:" << 10+5 << endl;
+	return 0;
+}
